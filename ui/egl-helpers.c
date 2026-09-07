1dcf85e5d2c70006295d9431e084529818fbe8c1
kagurasumusun <284823681+kagurasumusun@users.noreply.github.com>
2026-09-07 23:19:16 +0000

    wip: resolve the egl-helpers conflicts
    Keep UTM's ANGLE/IOSurface display setup (angle_native_device, buffer
    surfaces, cocoa dpy init) and restore qemu_egl_get_display() as a public
    helper because 11.0.3's ui/sdl2.c now calls it to probe for dma-buf.

--- ui/egl-helpers.c
+++ ui/egl-helpers.c
@@ -500,82 +500,10 @@ EGLSurface qemu_egl_init_surface(EGLContext ectx, EGLNativeWindowType win)
     return esurface;
 }
 
-<<<<<<< qemu-11.0.3-brain
-/* ---------------------------------------------------------------------- */
-
-#if defined(CONFIG_X11) || defined(CONFIG_GBM) || defined(WIN32)
-
-/*
- * Taken from glamor_egl.h from the Xorg xserver, which is MIT licensed
- *
- * Create an EGLDisplay from a native display type. This is a little quirky
- * for a few reasons.
- *
- * 1: GetPlatformDisplayEXT and GetPlatformDisplay are the API you want to
- * use, but have different function signatures in the third argument; this
- * happens not to matter for us, at the moment, but it means epoxy won't alias
- * them together.
- *
- * 2: epoxy 1.3 and earlier don't understand EGL client extensions, which
- * means you can't call "eglGetPlatformDisplayEXT" directly, as the resolver
- * will crash.
- *
- * 3: You can't tell whether you have EGL 1.5 at this point, because
- * eglQueryString(EGL_VERSION) is a property of the display, which we don't
- * have yet. So you have to query for extensions no matter what. Fortunately
- * epoxy_has_egl_extension _does_ let you query for client extensions, so
- * we don't have to write our own extension string parsing.
- *
- * 4. There is no EGL_KHR_platform_base to complement the EXT one, thus one
- * needs to know EGL 1.5 is supported in order to use the eglGetPlatformDisplay
- * function pointer.
- * We can workaround this (circular dependency) by probing for the EGL 1.5
- * platform extensions (EGL_KHR_platform_gbm and friends) yet it doesn't seem
- * like mesa will be able to advertise these (even though it can do EGL 1.5).
- */
-EGLDisplay qemu_egl_get_display(EGLNativeDisplayType native,
-                                EGLenum platform)
-||||||| qemu-10.0.12
-/* ---------------------------------------------------------------------- */
-
-#if defined(CONFIG_X11) || defined(CONFIG_GBM) || defined(WIN32)
-
-/*
- * Taken from glamor_egl.h from the Xorg xserver, which is MIT licensed
- *
- * Create an EGLDisplay from a native display type. This is a little quirky
- * for a few reasons.
- *
- * 1: GetPlatformDisplayEXT and GetPlatformDisplay are the API you want to
- * use, but have different function signatures in the third argument; this
- * happens not to matter for us, at the moment, but it means epoxy won't alias
- * them together.
- *
- * 2: epoxy 1.3 and earlier don't understand EGL client extensions, which
- * means you can't call "eglGetPlatformDisplayEXT" directly, as the resolver
- * will crash.
- *
- * 3: You can't tell whether you have EGL 1.5 at this point, because
- * eglQueryString(EGL_VERSION) is a property of the display, which we don't
- * have yet. So you have to query for extensions no matter what. Fortunately
- * epoxy_has_egl_extension _does_ let you query for client extensions, so
- * we don't have to write our own extension string parsing.
- *
- * 4. There is no EGL_KHR_platform_base to complement the EXT one, thus one
- * needs to know EGL 1.5 is supported in order to use the eglGetPlatformDisplay
- * function pointer.
- * We can workaround this (circular dependency) by probing for the EGL 1.5
- * platform extensions (EGL_KHR_platform_gbm and friends) yet it doesn't seem
- * like mesa will be able to advertise these (even though it can do EGL 1.5).
- */
-static EGLDisplay qemu_egl_get_display(EGLNativeDisplayType native,
-                                       EGLenum platform)
-=======
 EGLSurface qemu_egl_init_buffer_surface(EGLContext ectx,
                                         EGLenum buftype,
                                         EGLClientBuffer buffer,
                                         const EGLint *attrib_list)
->>>>>>> qemu-10.0.12-utm
 {
     EGLSurface esurface;
     EGLBoolean b;
